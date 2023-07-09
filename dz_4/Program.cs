3163aafc920c27aabdfc83d45d4f631cd37e8a0f
Maria Chernomazova <chernomazova@mari.bk>
2023-07-09 14:36:33 +0500

    Создали программу для задачи

---
+++ dz_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N>0), а на выходе показывает все четные числа от 1 до N.
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a>0){
+    for (int i=1; i<=a; i++){
+        if (i %2==0){
+            Console.WriteLine (i);
+        }
+    }
+}
+else {
+    Console.WriteLine("Число должно быть больше 0");
+}
